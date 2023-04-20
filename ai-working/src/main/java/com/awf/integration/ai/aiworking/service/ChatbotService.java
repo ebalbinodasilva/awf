293a0f71785e237cbaf4305be6b21ece3a873127
Everton <ebalbinodasilva@gmail.com>
2023-04-20 15:58:04 -0300

    fix Bean Spring boot

--- ai-working/src/main/java/com/awf/integration/ai/aiworking/service/ChatbotService.java
+++ ai-working/src/main/java/com/awf/integration/ai/aiworking/service/ChatbotService.java
@@ -1,6 +1,8 @@
 package com.awf.integration.ai.aiworking.service;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public interface ChatBotService {
 
     String sendMessage(String message);
--- ai-working/src/test/java/com/awf/integration/ai/aiworking/AiWorkingApplicationTests.java
+++ ai-working/src/test/java/com/awf/integration/ai/aiworking/AiWorkingApplicationTests.java
@@ -1,7 +1,6 @@
 package com.awf.integration.ai.aiworking;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 class AiWorkingApplicationTests {
